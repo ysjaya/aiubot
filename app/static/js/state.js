a0a6b2bdb4df62737688d5aad6b0c14526923fc6
oktetod <108335533+oktetod@users.noreply.github.com>
2025-09-30 01:54:02 +0800

    Create state.js

---
+++ app/static/js/state.js
@@ -0,0 +1,11 @@
+export const state = {
+    projects: [],
+    conversations: [],
+    files: [],
+    currentProjectId: null,
+    currentConvId: null,
+    isLoading: false,
+    ws: null,
+    githubToken: null,
+    selectedRepo: null,
+};
