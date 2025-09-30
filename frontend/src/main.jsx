062386f555aa8ffd6deb6d226afe3355c667c534
oktetod <108335533+oktetod@users.noreply.github.com>
2025-09-30 12:38:27 +0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
 
-ReactDOM.createRoot(document.getElementById('root')).createRoot(
+// BUG FIX: Jangan panggil createRoot dua kali!
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
